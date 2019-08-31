d2112070674552efb4eeb51f081f66f7f8c2aee8
beta1139 <beta1139@gmail.com>
2019-08-31 23:38:19 +0900

    no message

---
+++ FullscreenImage/app/src/main/java/develop/gamma2278/fullscreenimage/FullscreenImageActivity.kt
@@ -0,0 +1,12 @@
+package develop.gamma2278.fullscreenimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FullscreenImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
