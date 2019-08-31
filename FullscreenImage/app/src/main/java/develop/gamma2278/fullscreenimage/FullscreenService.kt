57e83cbb06f6d3b68f92cf76191b727603e138c1
beta1139 <beta1139@gmail.com>
2019-09-01 02:43:40 +0900

    no message

--- FullscreenImage/app/src/main/java/develop/gamma2278/fullscreenimage/FullscreenService.kt
+++ FullscreenImage/app/src/main/java/develop/gamma2278/fullscreenimage/FullscreenService.kt
@@ -6,11 +6,10 @@ import android.view.WindowManager
 
 object FullscreenService {
     fun hideNavigationBar(window: Window) {
-        window.decorView.systemUiVisibility =
-            (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+        window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
     }
 
     fun showStatusBar(window: Window) {
